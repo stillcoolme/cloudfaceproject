21529dd110ae32796e699ea60ca0c39e84e903dd
stillcoolme <stillcoolman@sina.com>
2019-10-16 19:29:11 +0800

    clear no used

--- face-client/src/main/java/com/grgbanking/faceclient/FaceClientApplication.java
+++ face-client/src/main/java/com/grgbanking/faceclient/FaceClientApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 // 向服务中心注册
 @EnableDiscoveryClient
-@EnableFeignClients
 public class FaceClientApplication {
 
     public static void main(String[] args) {
